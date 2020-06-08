edf4fd9cdf2d420a434a21536a3daae57997dd25
Rafael Garcia <rafagrcb@gmail.com>
2020-06-08 16:54:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,8 +24,8 @@ end
 
 summon_captain_planet(planeteer_calls)
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls
+  
 end
 
 def find_the_cheese# code an argument here
