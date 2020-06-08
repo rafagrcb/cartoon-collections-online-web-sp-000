76d99d617c025c5d0b4e7d19199ab0835cf3df86
Rafael Garcia <rafagrcb@gmail.com>
2020-06-08 16:48:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,8 @@ end
 
 roll_call_dwarves(dwarves)
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  
 end
 
 def long_planeteer_calls# code an argument here
