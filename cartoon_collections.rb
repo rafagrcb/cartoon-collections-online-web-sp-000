3f83d8786990bd3020340341e037a610243fabe9
Rafael Garcia <rafagrcb@gmail.com>
2020-06-08 16:55:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,7 +26,7 @@ summon_captain_planet(planeteer_calls)
 
 def long_planeteer_calls(array)
   i = 0
-  if array.any? {|i| i.length > 4
+  if array.any? {|i| i.length > 4}
 end
 
 def find_the_cheese# code an argument here
